ee15f509f1c826179edf69d00784efcc898bce24
walter4563 <walter456smith@outlook.com>
2024-08-30 17:46:02 +0530

    Create src_components_Receipt.js

---
+++ src_components_Receipt.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Receipt.css';
+
+function Receipt() {
+  return (
+    <div className="receipt">
+      <h2>Receipt</h2>
+      <p>Total: $0.00</p>
+    </div>
+  );
+}
+
+export default Receipt;
